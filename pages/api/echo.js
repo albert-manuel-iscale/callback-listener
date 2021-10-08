60abd95c03c9b1217bde8eb787dfa4711d49c917
albert-manuel-iscale <73093591+albert-manuel-iscale@users.noreply.github.com>
2021-10-08 10:37:10 +0800

    Log host and body

--- pages/api/echo.js
+++ pages/api/echo.js
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  console.log(req);
+  console.log({
+    host: req.host,
+    body: req.body,
+  });
   res.status(200).json(req);
 }
